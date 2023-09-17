2a5303ba555907cd144dd3c71a86544f62e04fcd
mamidelibas <mamidlbs@gmail.com>
2023-09-17 15:51:21 +0300

    react

--- mpapp/pages/index.js
+++ mpapp/pages/index.js
@@ -1,6 +1,12 @@
+import { useState } from "react";
+
 export default function Home() {
-  let nameSurname = "Muhammet Delibaş - ";
-  let age = 23;
+  const [firstName, setFirstName] = useState("Muhammet");
+
+  const [age, setAge] = useState(23);
+
+  // let firstName = "Muhammet Delibaş - ";
+  // let age = 23;
   let alertGoster = () => {
     alert("Merhaba");
     console.log("Butona tıklandı.");
@@ -12,15 +18,28 @@ export default function Home() {
   return (
     <>
       <h1>
-        Benim adım : {nameSurname} Yaşım : {age}
+        Benim adım : {firstName} Yaşım : {age}
       </h1>
       {/* OBJELERİN İÇİNDEKİ VERİYİ ÇEKME */}
       <h2>{studenObject.studenAge}</h2>
-      <button onClick={alertGoster}>Click</button> <br /> <br />
+      <button
+        onClick={() => {
+          alertGoster();
+          console.log(firstName);
+        }}
+      >
+        Click
+      </button>{" "}
+      <br /> <br />
       <input
         placeholder="Lütfen Bir Metin Yazın..."
-        onChange={(e) => {
-          console.log(e.target.value);
+        onChange={(event) => {
+          // console.log(e.target.value);
+          // firstName = event.target.value;
+
+          // inputa girilen değeri al ve Click fonksiyonunda değiştir
+          // setFirstName(event.target.value);
+          setAge(event.target.value);
         }}
       />
     </>
