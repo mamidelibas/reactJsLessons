9987f3240b37b19224ca22aa81834ee759718a38
mamidelibas <mamidlbs@gmail.com>
2023-09-17 15:18:18 +0300

    react

--- mpapp/pages/index.js
+++ mpapp/pages/index.js
@@ -5,12 +5,24 @@ export default function Home() {
     alert("Merhaba");
     console.log("Butona tıklandı.");
   };
+  let studenObject = {
+    studentName: "Ahmet",
+    studenAge: 23,
+  };
   return (
     <>
       <h1>
         Benim adım : {nameSurname} Yaşım : {age}
       </h1>
-      <button onClick={alertGoster}>Click</button>
+      {/* OBJELERİN İÇİNDEKİ VERİYİ ÇEKME */}
+      <h2>{studenObject.studenAge}</h2>
+      <button onClick={alertGoster}>Click</button> <br /> <br />
+      <input
+        placeholder="Lütfen Bir Metin Yazın..."
+        onChange={(e) => {
+          console.log(e.target.value);
+        }}
+      />
     </>
   );
 }
